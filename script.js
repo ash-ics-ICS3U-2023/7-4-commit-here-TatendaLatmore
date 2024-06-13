e97c8ad7c31d5634163457c4e252279a46fe5d6a
Tatenda <159183456+TatendaLatmore@users.noreply.github.com>
2024-06-13 13:32:47 -0400

    clicker

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 /**
  * This file will get loaded by index.html
  */
+
+document.getElementById('incrementButton').addEventListener('click', () => {
+    clicks++;
+    document.getElementById('clicks').innerText = clicks;
+});
+
+document.getElementById('decrementButton').addEventListener('click', () => {
+    clicks--;
+    document.getElementById('clicks').innerText = clicks;
+});
+
+document.getElementById('resetButton').addEventListener('click', () => {
+    clicks = 0;
+    document.getElementById('clicks').innerText = clicks;
+});
